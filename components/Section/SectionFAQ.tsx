d97026c65d5dcd9cd3629b8aa96905cf88bb179b
Mike Sindani <mikems@live.fr>
2024-07-18 15:46:07 +0200

    adapter for production

--- components/Section/SectionFAQ.tsx
+++ components/Section/SectionFAQ.tsx
@@ -27,13 +27,13 @@ const SectionFAQ = () => {
             <AccordionItem value="item-1">
               <AccordionTrigger> Qui sommes-nous?</AccordionTrigger>
               <AccordionContent>
-              Nous sommes AutoPro, votre partenaire automobile de confiance. Nous nous engageons à offrir une expérience d'achat transparente et sans tracas, que ce soit pour une voiture neuve ou d'occasion.
+              Nous sommes AutoPro, votre partenaire automobile de confiance. Nous nous engageons à offrir une expérience d{"'"}achat transparente et sans tracas, que ce soit pour une voiture neuve ou d{"'"}occasion.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
               <AccordionTrigger>Quel est notre engagement envers nos clients?</AccordionTrigger>
               <AccordionContent>
-              Nous nous engageons à offrir une expérience d {"'"}achat transparente et sans tracas à nos clients, que ce soit pour une voiture neuve ou d'occasion.
+              Nous nous engageons à offrir une expérience d {"'"}achat transparente et sans tracas à nos clients, que ce soit pour une voiture neuve ou d{"'"}occasion.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
@@ -41,13 +41,13 @@ const SectionFAQ = () => {
               Qu est-ce que la transparence dans le processus d achat d une voiture?
               </AccordionTrigger>
               <AccordionContent>
-              La transparence signifie que nous fournirons aux clients toutes les informations nécessaires sur le véhicule, notamment son historique, son état et son prix, pour qu'ils puissent prendre une décision éclairée.
+              La transparence signifie que nous fournirons aux clients toutes les informations nécessaires sur le véhicule, notamment son historique, son état et son prix, pour qu{"'"}ils puissent prendre une décision éclairée.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-4">
               <AccordionTrigger>Comment assurons-nous une expérience sans tracas?</AccordionTrigger>
               <AccordionContent>
-              Nous visons à simplifier le processus d'achat de voiture en minimisant les formalités, en fournissant des informations claires et concises, et en offrant un processus d'achat fluide.
+              Nous visons à simplifier le processus d{"'"}achat de voiture en minimisant les formalités, en fournissant des informations claires et concises, et en offrant un processus d{"'"}achat fluide.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-5">
@@ -55,7 +55,7 @@ const SectionFAQ = () => {
               Quels types de véhicules proposons-nous? 
               </AccordionTrigger>
               <AccordionContent>
-              Nous proposons à la fois des voitures neuves et des voitures d occasion, pour que les clients puissent choisir celle qui convient le mieux à leurs besoins et à leur budget.
+              Nous proposons à la fois des voitures neuves et des voitures d{"'"}occasion, pour que les clients puissent choisir celle qui convient le mieux à leurs besoins et à leur budget.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-6">
@@ -68,10 +68,10 @@ const SectionFAQ = () => {
             </AccordionItem>
             <AccordionItem value="item-7">
               <AccordionTrigger>
-              Que se passe-t-il si j ai des questions ou des préoccupations pendant le processus d achat?
+              Que se passe-t-il si j{"'"} ai des questions ou des préoccupations pendant le processus d achat?
               </AccordionTrigger>
               <AccordionContent>
-              Notre personnel amical et compétent est disponible pour répondre à toutes vos questions ou préoccupations, pour vous assurer de vous sentir confiant et à l'aise tout au long du processus.
+              Notre personnel amical et compétent est disponible pour répondre à toutes vos questions ou préoccupations, pour vous assurer de vous sentir confiant et à l{"'"}aise tout au long du processus.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
