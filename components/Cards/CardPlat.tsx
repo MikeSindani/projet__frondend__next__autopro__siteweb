af2c2318981e42d82b1afc5e06c0016234ea13cb
Mike Sindani <mikems@live.fr>
2024-08-07 14:22:12 +0200

    update site and remove bg form voiture section catalogue

--- components/Cards/CardPlat.tsx
+++ components/Cards/CardPlat.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from 'react';
 import ScrollAnimation from "react-animate-on-scroll";
+import './style.css'
 
 // Définition du type pour un plat
 interface Car {
@@ -60,13 +61,13 @@ const CardPlat: React.FC<DishCarouselProps> = ({ data, href }) => {
   // <div className="flex justify-start items-center overflow-x-scroll overflow-y-hidden ">
   return (
     <section className="w-full">
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 md:gap-2">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 md:gap-3">
       {data.map((dish, index) => (
         <ScrollAnimation key={index} animateIn="fadeIn" delay={300}>
         <div
-          className="flex-shrink-0 w-40 md:w-64  rounded-xl shadow-lg bg-white h-full hover:scale-95 transition-all duration-200"
+          className="element flex-shrink-0 w-40 md:w-64  rounded-xl  bg-white h-full hover:scale-105 transition-all duration-200"
         >
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center bg-orange-100">
             <Image
               className={"h-64 md:h-52 w-full object-cover mb-4 rounded-xl"}
               src={dish.images[0]}
@@ -77,7 +78,7 @@ const CardPlat: React.FC<DishCarouselProps> = ({ data, href }) => {
           </div>
           <div className="flex flex-col p-1 text-center">
             <h1 className="text-2xl font-bold m-1 text-gray-900">{`${dish.marque}`}</h1>
-            <h3 className="text-lg flex gap-1  justify-center items-center flex-col">
+            <h4 className="text-md flex gap-1  justify-center items-center flex-col">
               <p className="text-primary-color font-bold text-center hidden">
                 {formatedNumber(dish.priceUSD)}$
               </p>
@@ -87,7 +88,7 @@ const CardPlat: React.FC<DishCarouselProps> = ({ data, href }) => {
               <p className="text-slate-600 font-light text-center text-sm hidden">
                 {convertUSDToCDF(dish.priceUSD) || dish.priceCDF} francs congolais
               </p>
-            </h3>
+            </h4>
             <div className="divide-x-2 flex space-x-2 my-3 mx-1 justify-center items-center hidden">
               <InCardDetails title="Moteur" desc={dish.moteur}></InCardDetails>
               <InCardDetails title="Trans." desc={dish.transmission}></InCardDetails>
@@ -100,13 +101,13 @@ const CardPlat: React.FC<DishCarouselProps> = ({ data, href }) => {
               <p className="text-gray-900 text-xs hidden">{dish.etat}</p>
             </div>
           </div>
-          <div className="flex flex-col gap-2 justify-center m-2">
-            <button 
-              onClick={()=>handleModal(dish.id)}
+          <div className="flex flex-col gap-2 justify-center m-2 info transition-opacity opacity-0 hidden">
+            <a
+              href="https://wa.me/243992099990?text=Bonjour%2C+je+suis+int%C3%A9ress%C3%A9+par+vos+voitures.+Pouvez-vous+m%27expliquer+comment+l%27obtenir+et+les+d%C3%A9tails+de+l%27achat%3F+svp"
               className=" hover:bg-gray-900 hover:text-white w-full text-center py-2 rounded-full text-sm font-medium bg-primary-color text-white"
             >
-              Voir plus
-            </button>
+              contactez-nous
+            </a>
           </div>
         </div>
         </ScrollAnimation>
